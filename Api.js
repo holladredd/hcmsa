43efb101b2410c1eb37b634bdaa035497dca488c
olamide folayan <holladred@gmail.com>
2025-01-15 18:04:02 +0100

    update

--- Api.js
+++ Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:5000/api";
+// const API_URL_AUTH = "https://hcmsaserver.onrender.com";
 
 // Create axios instance with default config
 const api = axios.create({
